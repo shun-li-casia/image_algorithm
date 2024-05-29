5d92d70431b08f035a14163a8e03ed79742d429a
Shun Li <shun.li.at.casia@outlook.com>
2024-05-29 16:18:47 +0800

    u

---
+++ src/tools/stereo_disparity.cc
@@ -0,0 +1,14 @@
+/*******************************************************************************
+ *   Copyright (C) 2023 CASIA. All rights reserved.
+ *
+ *   @Filename: stereo_disparity.cc
+ *
+ *   @Author: shun li
+ *
+ *   @Email: shun.li.at.casia@outlook.com
+ *
+ *   @Date: 29/05/2024
+ *
+ *   @Description: calculate the stereo disparity from the rect image.
+ *
+ *******************************************************************************/
